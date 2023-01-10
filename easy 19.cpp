b0211c813b216e45f1da0cd37f0ef068d33d5ae3
Shivaashok <122215006+Shivaashok@users.noreply.github.com>
2023-01-11 05:13:21 +0530

    Add files via upload

---
+++ easy 19.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+	int a,b,*c,*d;
+	printf("Enter two numbers : ");
+	scanf("%d%d",&a,&b);
+	c=&a,d=&b;
+	if(*c>*d)
+	printf("%d is the greater number ",a);
+	else
+	printf("%d is the greater number ",b);
+}
+
--- easy 21.cpp
+++ easy 21.cpp
@@ -3,17 +3,21 @@
 #include<math.h>
 int main()
 {
-	int c,a,b;
-	int adg(int*var,int *vare);
-	//add numbers by referance 
-	printf("Enter : ");
-	scanf("%d%d",&a,&b);
-	c=adg(&a,&b);
-	printf("d %d",c);
+	int a[]={15, 14, 25, 14, 32, 14, 31},i,j,n,b,c,d;
+	n=sizeof(a)/sizeof(a[0]);
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=a[i];
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	printf("%d",a[i]);
 }
-int adg(int *var,int *vare)
-{
-	int a1,b1;
-	a1=*var+*vare;
-	return (a1);	
-}
+
